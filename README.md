e44ec0ae6b9e1ab2b39640fccf698e15daba7f9c
chaiyut-kun <chwork28@hotmail.com>
2026-03-20 08:50:37 +0700

    Initial commit: initial mobile app for final quiz

---
+++ README.md
@@ -0,0 +1,2 @@
+# Overview
+This project is making for final quiz native mobile subject
---
+++ android/app/src/main/kotlin/com/example/flutter_final_quiz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_final_quiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
